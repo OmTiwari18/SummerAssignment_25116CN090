c35cfacd9143e8a77d43f5d587935ee69e8f2058
OmTiwari18 <omtiwary3011@gmail.com>
2026-05-29 23:56:11 +0530

    Add files via upload

---
+++ dayONE.c
@@ -0,0 +1,80 @@
+                                // QESTION NO.1   
+
+
+// #include<stdio.h>
+// int main()
+// {
+//    int n , sum = 0 ,i;
+//    printf (" enter the limit of number ");
+//    scanf (" %d ", &n);
+//    for (i=1 ; i<=n ; i++)
+//    {
+//      sum = sum + i;
+//      printf (" sum is %d ",sum);
+//    }
+//    return 0; }
+
+
+
+                            //   QUESTION 2 
+
+        
+// #include<stdio.h>
+// void main()
+// {
+//     int n ,i ;
+//     printf (" enter the number for which table has to be printed ");
+//     scanf("%d",&n);
+//     printf(" the multiplication table of the tiven no is %d" ,n );
+//     for ( i=1;i<=n;i++)
+//     {
+//         printf( " %d * %d = %d " , n , i , n*i );
+    
+//     }
+//}
+
+
+
+                                //  QUESTION 3
+
+
+// #include<stdio.h>
+// int main() 
+// {
+//     int n , fact =1 ,i ;
+//     printf( " give the number till where the factorial has to be printed ");
+//     scanf("%d" , n );
+//     for( i=1 ; i<=n; i++)
+//     {
+//         fact= fact*i;
+//         printf (" the factorial of given no is %d ", fact);
+//     }
+//     return 0;
+// }
+
+
+
+                                    // QUESTION 4
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//    int n , d=0 ;
+//    printf (" enter the number whose digit is to be count");
+//    scanf(" %d", n );
+//    if(n==0)
+//    {
+//     printf (" wrong input ");
+//    }
+//    else
+//    {
+//     while (n>0)
+//     {
+//         n=n/10;
+//         d++;
+//     }
+//     printf(" the no of digits are %d" ,d );
+//    }
+// }
